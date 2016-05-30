622ae3b5df33abfde5b1ff8bf143301e51c154b5
Akshay Kumar Gupta <akshay.akgupta@gmail.com>
2016-05-30 17:00:08 -0400

    Added toy dataset

---
+++ data/data.py
@@ -0,0 +1,33 @@
+import theano
+import theano.tensor as T
+import numpy as np
+
+from utils import Container
+
+def get_boston_data(op):
+
+    dt = Container()
+    DT = []
+    with open('housing.data', 'rb') as f:
+        for line in f:
+            line = [float(val) for val in line.strip().split()]
+            DT.append(line)
+    DT = np.asarray(DT).astype(theano.config.floatX)
+    
+    # data standardization
+    dt.mu  = np.mean(DT, axis=0)
+    dt.sig = np.std(DT, axis=0)
+    DT = (DT - dt.mu) / dt.sig 
+    dt.no_stdz = lambda val: val * dt.sig[-1] + dt.mu[-1]
+    
+    # shuffle
+    shuffle_idx = np.random.permutation(DT.shape[0])
+    DT = DT[shuffle_idx]
+
+    dt.trn_X = DT[:op.trn_sz,:-1]
+    dt.trn_Y = DT[:op.trn_sz,-1]
+    dt.val_X = DT[op.trn_sz:,:-1]
+    dt.val_Y = DT[op.trn_sz:,-1]
+
+    return dt
+
