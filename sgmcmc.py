83ded8e95f5a966b001fc8889de104cefe0fa00c
Akshay Kumar Gupta <akshay.akgupta@gmail.com>
2016-06-02 13:40:03 -0400

    Added sgld

--- sgmcmc.py
+++ sgmcmc.py
@@ -2,8 +2,25 @@ from __future__ import print_function
 
 import theano
 from theano import tensor
+from theano.sandbos.rng_mrg import MRG_RandomStreams as RandomStreams
 
 import numpy as np
+
+pi = 3.141592653
+trng = RandomStreams(1234)
+
+def log_prior_normal(model_params, prec):
+    res = 0.
+    for p in model_params:
+        res += tensor.sum(- 0.5 * (tensor.log(2 * pi / prec) + prec * tensor.sqr(p)))
+    return res
+
+def logpdf_normal(pp, yy, prec_lik):
+    logliks = - 0.5 * (np.log(2 * pi / prec_lik) + prec_lik * (pp - yy)**2) 
+    return logliks
+
+def compute_rmse(xx, yy):
+    return np.sqrt(((xx - yy)**2).mean())
 
 def train(model, data, params):
     '''Train the model.
@@ -12,13 +29,13 @@ def train(model, data, params):
     ----------
     model: Container
     The model to train. Should have the following attributes:
-    model.xx : theano.tensor.matrix  (for input)
-    model.yy : theano.tensor.vector  (for output)
-    model.params : flat dictionary of model parameters
+    model.xx : theano.tensor.matrix  (the input variable)
+    model.pp : theano.tensor.vector  (the model output variable)
+    model.params : list (list of model parameters as theano shared variables)
 
     data: Container
     The data to train on. Should have the following attributes:
-    data.trn_X : np.array
+    data.trn_X : np.array ()
     data.trn_Y : np.array
     data.val_X : np.array
     data.val_Y : np.array
@@ -30,35 +47,37 @@ def train(model, data, params):
 
     is_sgd_mode = tensor.fscalar('is_sgd_mode')
     lr = tensor.fscalar('lr')
+    yy = tensor.vector('yy', dtype='float32')
 
     if params.algo == 'sgld':
-        updates, log_likelihood, pp = sgld(model, params)
+        updates, log_likelihood = sgld(model, yy, params)
     elif params.algo == 'sgfs':
-        updates, log_likelihood, pp = sgfs(model.params, params)
+        updates, log_likelihood = sgfs(model, yy, params)
     elif params.algo == 'sgrld':
-        updates, log_likelihood, pp = sgrld(model.params, params)
+        updates, log_likelihood = sgrld(model, yy, params)
     elif params.algo == 'sghmc':
-        updates, log_likelihood, pp = sghmc(model.params, params)
+        updates, log_likelihood = sghmc(model, yy, params)
     elif params.algo == 'sgnht':
-        updates, log_likelihood, pp = sgnht(model.params, params)
+        updates, log_likelihood = sgnht(model, yy, params)
     elif params.algo == 'psgld':
-        updates, log_likelihood, pp = psgld(model.params, params)
+        updates, log_likelihood = psgld(model, yy, params)
     else:
         raise UnknownArgumentException
 
     fn = Container()
-    fn.train = theano.function(inputs = [xx, yy, lr, is_sgd_mode],
-                               outputs = [pp, log_likelihood],
-                               updates = updates,
-                               on_unused_input = 'warn')
-    fn.val = theano.function(inputs = [xx],
-                             outputs =[pp],
-                             on_unused_input = 'warn')
+    fn.backprop = theano.function(inputs = [model.xx, yy, lr, is_sgd_mode],
+                                  outputs = [model.pp, log_likelihood],
+                                  updates = updates,
+                                  on_unused_input = 'warn')
+    fn.forward = theano.function(inputs = [model.xx],
+                                 outputs =[model.pp],
+                                 on_unused_input = 'warn')
 
     avg_pp = np.zeros(data.val_Y.shape)
     sum_pp = np.zeros(data.val_Y.shape)
     sumsq_pp = np.zeros(data.val_Y.shape)
     val_avg_log_likelihood = 0.0
+    n_samples = 0
 
     if not params.is_sgd_mode:
         sampling = False
@@ -69,7 +88,9 @@ def train(model, data, params):
         mini_idx = np.floor(np.random.rand(params.batch_sz) * data.train_size).astype('int32')
         mini_X = data.trn_X[mini_idx]
         mini_Y = data.trn_Y[mini_idx]
-        train_pp, log_likelihood = fn.train(mini_X, mini_Y, params.lr, params.is_sgd_mode)
+
+        #parameter update
+        (train_pp, log_likelihood) = fn.backprop(mini_X, mini_Y, params.lr, params.is_sgd_mode)
 
         if i == params.burnin and not params.is_sgd_mode:
             #burnin period over, begin sampling
@@ -79,9 +100,66 @@ def train(model, data, params):
             lr = lr / 2
 
         if i % thinning == 0:
-            val_pp = fn.val(data.val_X, data.val_Y)
-            
+            val_pp = fn.forward(data.val_X)
 
+            if sampling:
+                n_samples += 1
+                # prediction based on current parameter (sample)
+                avg_pp = ((1 - (1./n_samples)) * avg_pp) + ((1./n_samples) * val_pp)
+                ppp = avg_pp
+                # online sample variance 
+                sum_pp += val_pp
+                sumsqr_pp += val_pp * val_pp
+                var_pp = (sumsqr_pp - (sum_pp * sum_pp)/n_samples) / (n_samples - 1)
+            else:
+                ppp = val_pp
 
+                trn_pp = fn.forward(dt.trn_X) # train predictions
+                var_pp = np.var(trn_pp - data.trn_Y)
 
+            mean_ll = logpdf_normal(ppp, val_yy, 1/var_pp).mean()
+            rmse = compute_rmse(ppp, val_yy)
 
+            print '%d/%d, %.2f, %.2f (%.2f)  \r' % \
+                    (i, n_samples, sumloglik, meanlogliks, rmse),
+
+    print '%d/%d, %.2f, %.2f (%.2f)' % \
+            (i, n_samples, sumloglik, meanlogliks, rmse)
+
+def grad_clipping(model_params, grads, gc_norm=10.):
+    norm = ut.norm_gs(model_params, grads)
+    sqrtnorm = T.sqrt(norm)
+    adj_norm_gs = T.switch(T.ge(sqrtnorm, gc_norm), 
+                           gc_norm / sqrtnorm, 1.)
+    return adj_norm_gs
+
+def sgld(model, yy, params):
+    '''Stochastic Gradient Langevin Dynamics
+    Implemented according to the paper:
+    Welling, Max, and Yee W. Teh., 2011
+    "Bayesian learning via stochastic gradient Langevin dynamics."
+    '''
+
+    logliks = - 0.5 * (tensor.log(2 * pi / params.prec_lik) + params.prec_lik * (model.pp - yy)**2) 
+    logprior = log_prior_normal(model.params, op.prec_prior)
+    sumloglik = logliks.sum()
+    logpost = N * sumloglik / n + logprior
+
+    #compute gradients
+    grads = tensor.grad(cost=logpost,  wrt=params)
+
+    # gradient clipping 
+    if params.gc_norm is not None:
+        adj_norm_gs = grad_clipping(model.params, grads, params.gc_norm)  
+    else:
+        adj_norm_gs = 1.0
+
+    updates = []
+    for p, g in zip(model.params, grads):
+        grad = g * adj_norm_gs 
+        # noise is discarded in sgd mode which may be used during burnin
+        noise = ifelse(tensor.eq(sgd_mode, 1.), 
+                       tensor.alloc(0., *p.shape),
+                       tensor.sqrt(params.lr) * trng.normal(p.shape, avg=0.0, std=1.0))
+        updates.append((p, p + 0.5 * params.lr * grad + noise))
+    return updates    
