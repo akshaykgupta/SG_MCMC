3a0b67242250124a150a34ea558815fe5f4af324
Akshay Kumar Gupta <akshay.akgupta@gmail.com>
2016-06-02 13:40:03 -0400

    Added sgld

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from . import data
+from . import datasets
--- datasets/boston_housing.py
+++ datasets/boston_housing.py
@@ -12,21 +12,22 @@ def get_data(path, op):
             line = [float(val) for val in line.strip().split()]
             DT.append(line)
     DT = np.asarray(DT).astype(theano.config.floatX)
-    
-    # data standardization
-    dt.mu  = np.mean(DT, axis=0)
-    dt.sig = np.std(DT, axis=0)
-    DT = (DT - dt.mu) / dt.sig 
-    dt.no_stdz = lambda val: val * dt.sig[-1] + dt.mu[-1]
-    
+
     # shuffle
     shuffle_idx = np.random.permutation(DT.shape[0])
     DT = DT[shuffle_idx]
 
-    dt.trn_X = DT[:op.train_size,:-1]
-    dt.trn_Y = DT[:op.train_size,-1]
-    dt.val_X = DT[op.train_size:,:-1]
-    dt.val_Y = DT[op.train_size:,-1]
+    DT_x = DT[:, :-1]
+    DT_y = DT[:, -1]
+    # data standardization
+    dt.mu  = np.mean(DT_x, axis=0)
+    dt.sig = np.std(DT_x, axis=0)
+    DT_x = (DT_x - dt.mu) / dt.sig
+    
+    dt.trn_X = DT_x[:op.train_size]
+    dt.trn_Y = DT_y[:op.train_size]
+    dt.val_X = DT_x[op.train_size:]
+    dt.val_Y = DT_y[op.train_size:]
 
     return dt
 
