c06f44fedd2e1b24caed5cb70c10a9ec12689693
Akshay Kumar Gupta <akshay.akgupta@gmail.com>
2016-06-15 00:03:00 -0400

    SGFS related bug fixes

--- sgmcmc.py
+++ sgmcmc.py
@@ -260,11 +260,12 @@ class SGFS(Trainer):
 
     def _create_auxiliary_variables(self):
         self.lr = tensor.scalar('lr')
-        n_params = 0
+        self.n_weights = 0
         for p in self.weights:
-            n_params += tensor.prod(p.shape)
-        self.I_t = theano.shared(np.asarray(np.random.randn(n_params, n_params),
+            self.n_weights += np.prod(p.get_value().shape)
+        self.I_t = theano.shared(np.asarray(np.zeros((self.n_weights, self.n_weights)),
                                             dtype = theano.config.floatX))
+        self.it = theano.shared(1.)
 
     def _get_updates(self):
         n = self.params['batch_size']
@@ -272,7 +273,7 @@ class SGFS(Trainer):
         prec_lik = self.params['prec_lik']
         prec_prior = self.params['prec_prior']
         gc_norm = self.params['gc_norm']
-        gamma = (n + N) / n
+        gamma = float(n + N) / n
 
         # compute log-likelihood
         error = self.model_outputs - self.true_outputs
@@ -281,6 +282,7 @@ class SGFS(Trainer):
 
         # compute gradient of likelihood wrt each data point
         grads = tensor.jacobian(expression = logliks, wrt = self.weights)
+        grads = tensor.concatenate([g.flatten(ndim=2) for g in grads], axis=1)
         avg_grads = grads.mean(axis = 0)
         dist_grads = grads - avg_grads
 
@@ -289,33 +291,37 @@ class SGFS(Trainer):
 
         logprior = log_prior_normal(self.weights, prec_prior)
         grads_prior = tensor.grad(cost = logprior, wrt = self.weights)
+        grads_prior = tensor.concatenate([g.flatten() for g in grads_prior])
 
         # update Fisher information
-        I_t_next = (1 - self.lr) * self.I_t + self.lr * var_grads
+        I_t_next = (1 - 1/self.it) * self.I_t + 1/self.it * var_grads
 
         # compute noise
         if 'B' in self.params:
             B = self.params['B']
         else:
-            B = gamma * I_t_next
+            B = gamma * I_t_next * N
+        # B += np.eye(self.n_weights) * (10 ** -9)
         B_ch = slinalg.cholesky(B)
-        noise = tensor.dot(((2. / tensor.sqrt(lr)) * B_ch), 
-                           trng.normal(grads.shape))
+        noise = tensor.dot(((2./tensor.sqrt(self.lr)) * B_ch),
+                           trng.normal((self.n_weights, 1)))
 
         # expensive inversion
         inv_cond_mat = gamma * N * I_t_next + (4./self.lr) * B
-        cond_mat = nlinalg.matrix_inverse(inv_condition_mat)
+        cond_mat = nlinalg.matrix_inverse(inv_cond_mat)
 
         updates = []
         updates.append((self.I_t, I_t_next))
+        updates.append((self.it, self.it + 1))
 
         # update the parameters
-        updated_params = 2 * tensor.dot(cond_mat, (grads_prior + N * avg_grads + noise))
+        updated_params = 2 * tensor.dot(cond_mat, grads_prior + N * avg_grads + noise.flatten())
+        updated_params = updated_params.flatten()
         last_row = 0
         for p in self.weights:
-            sub_index = tensor.prod(p.shape)
-            up = updated_params[last_row:sub_index]
-            up.reshape(p.shape)
+            sub_index = np.prod(p.get_value().shape)
+            up = updated_params[last_row:last_row+sub_index]
+            up = up.reshape(p.shape)
             updates.append((p, up))
             last_row += sub_index
 
